1c299d25238f54f8a38f367fc9fc9a602238f1b2
zackadoodledoo <sin12014@byui.edu>
2026-03-04 11:17:36 -0800

    Full project clean up and validation

--- validation/workoutValidation.js
+++ validation/workoutValidation.js
@@ -1,8 +1,21 @@
 import { check } from 'express-validator';
 
 export const workoutValidationRules = [
-  check('name').trim().notEmpty().withMessage('Name is required'),
-  check('type').trim().notEmpty().withMessage('Type is required'),
-  check('duration').isNumeric().withMessage('Duration must be a number'),
-  check('calories').isNumeric().withMessage('Calories must be a number')
+  check('name')
+    .trim()
+    .notEmpty().withMessage('Name is required')
+    .isString().withMessage('Name must be a string'),
+
+  check('type')
+    .trim()
+    .notEmpty().withMessage('Type is required')
+    .isString().withMessage('Type must be a string'),
+
+  check('duration')
+    .notEmpty().withMessage('Duration is required')
+    .isNumeric().withMessage('Duration must be a number'),
+
+  check('calories')
+    .notEmpty().withMessage('Calories is required')
+    .isNumeric().withMessage('Calories must be a number')
 ];

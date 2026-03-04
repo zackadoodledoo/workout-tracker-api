9560a212d6b1a7d6ac227eedc268b146d9b81e4a
zackadoodledoo <sin12014@byui.edu>
2026-03-04 12:24:04 -0800

    Fixxin' swagger

--- swagger/swagger.js
+++ swagger/swagger.js
@@ -6,7 +6,17 @@ const doc = {
     description: 'API Documentation for my workout tracker endpoints'
   },
   host: 'localhost:3000',
-  schemes: ['http']
+  schemes: ['http'],
+  servers: [
+    {
+      url: 'http://localhost:3000',
+      description: 'Local development server'
+    },
+    {
+      url: 'https://workout-tracker-api-ns8z.onrender.com',
+      description: 'Production server (Render)'
+    }
+  ]
 };
 
 const outputFile = './swagger.json';
